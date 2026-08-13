90bed230e5c790d1c61c208f4ab6e8a9e088ebbe
Marcos Attard <marcosrattard@gmail.com>
2026-08-13 10:38:30 -0300

    [joinha] update pwa icons to v3

--- vite.config.ts
+++ vite.config.ts
@@ -15,18 +15,22 @@ export default defineConfig(() => ({
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: [
-        "favicon.ico",
-        "joinha-loja-icon-v2.png",
-        "joinha-staff-icon-v2.png",
-        "manifest-loja.webmanifest",
-        "manifest-staff.webmanifest",
-        "OneSignalSDKWorker.js",
-        "robots.txt"
-      ],
+  "favicon.ico",
+  "joinha-loja-icon-v3-192.png",
+  "joinha-loja-icon-v3-512.png",
+  "joinha-loja-icon-v3-1024.png",
+  "joinha-staff-icon-v3-192.png",
+  "joinha-staff-icon-v3-512.png",
+  "joinha-staff-icon-v3-1024.png",
+  "manifest-loja.webmanifest",
+  "manifest-staff.webmanifest",
+  "OneSignalSDKWorker.js",
+  "robots.txt"
+],
       manifest: false,
       workbox: {
         navigateFallback: "/index.html",
-        cacheId: "joinha-pwa-v2",
+        cacheId: "joinha-pwa-v3",
         cleanupOutdatedCaches: true
       }
     }),
