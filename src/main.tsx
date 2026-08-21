8f8aaf915fa3ee9fdcc646b89a9136f4c5db93c9
Marcos Attard <marcosrattard@gmail.com>
2026-08-21 10:54:01 -0300

    Remove PWA service worker registration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,4 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import { registerSW } from "virtual:pwa-register";
-
-registerSW({
-  immediate: true,
-});
-
 createRoot(document.getElementById("root")!).render(<App />);
